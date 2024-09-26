b37795c77efe76885f4d4f1d7f5ec478ea3bc817
Kiril <130992245+Kiril2008@users.noreply.github.com>
2024-09-26 21:51:46 +0300

    2main.cpp
    add SetConsole , add #include <windows.h>

--- main.cpp
+++ main.cpp
@@ -1,20 +1,30 @@
 #include <iostream>
-#include <conio.h>  // Для використання _getch()
+#include <conio.h>
+#include<windows.h>
+
 using namespace std;
 
-int main()
-{
-    cout << "Hello!" << endl;  // endl – перехід на новий рядок
-    cout << "I am an industrial college student" << endl;  // Виправлено cout
+int main(){
+
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
     int a, b;
-    cout << "Enter an integer (a): ";
+    cout << " Введіть ціле число (a): ";
     cin >> a;
-    cout << "Enter an integer (b): ";
+    cout << " Введіть ціле число (b): ";
     cin >> b;
-    cout << a << " + " << b << " = " << a + b << endl;
 
-    _getch();  // Очікування натискання клавіші перед завершенням програми
+    cout << "Сума: " << a << " + " << b << " = " << a + b << endl;
+    if (b != 0) {
+        cout << "Частка: " << a << " / " << b << " = " << a / b << endl;
+        cout << "Остача: " << a % b << endl;
+        cout << "Різниця частки та остачі: " << a / b - a % b << endl;
+        cout << "Добуток частки та остачі: " << a / b * a % b << endl;
+    } else {
+        cout << "Помилка! ділення на нуль!" << endl;
+    }
 
+    _getch();
     return 0;
 }
-
